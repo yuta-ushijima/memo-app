5e3f99723b0e306662f9551d112c215c03f3c1b9
yuta-ushijima <register@yuta-u.com>
2018-09-24 21:24:38 +0900

    Define UserMutation of CRUD

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -6,5 +6,9 @@ module Types
     def test_field
       "Hello World"
     end
+
+    field :create_user, mutation: Mutations::CreateUser
+    field :update_user, mutation: Mutations::UpdateUser
+    field :delete_user, mutation: Mutations::DeleteUser
   end
 end
