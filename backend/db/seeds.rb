dc9ad07ac9f734cb9d7da8221763ec2a8b4a5d31
yuta-ushijima <register@yuta-u.com>
2018-09-24 17:13:16 +0900

    Define Relation and Create db:seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  user = User.create user_name: Faker::Name.name,
+                       email: Faker::Internet.email,
+                       password_digest: Faker::Internet.password(6, 8)
+
+  5.times do
+    user.articles.create title: Faker::Coffee.blend_name,
+                        body: Faker::Coffee.notes
+  end
+end
+
+puts "seed is completed"
